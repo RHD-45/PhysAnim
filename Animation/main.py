66794b40d31643e77b95638927c80f7d707bb980
RHD-45 <vonguyentanthanh@gmail.com>
2025-07-03 10:38:55 +0700

    init

---
+++ Animation/main.py
@@ -0,0 +1,23 @@
+from manim import *
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+class AMSLaTeX(Scene):
+    def construct(self):
+        tex = Tex(r'$\mathtt{H} \looparrowright$ \LaTeX', font_size=50)
+        self.add(tex)
